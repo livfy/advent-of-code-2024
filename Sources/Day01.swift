e6092cffae972d267aff13490b965c21dd9d72c9
Liviru <119770778+livfy@users.noreply.github.com>
2024-12-02 15:38:27 +1100

    Solve day 1 part 1 puzzle

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -1,17 +1,50 @@
 import Algorithms
 
+extension [Int] {
+    var second: Int? {
+        self[1]
+    }
+}
+
 struct Day01: AdventDay {
     var data: String
 
-    var entities: [[Int]] {
+    var locations: [[Int]] {
         data.split(separator: "\n").map {
             $0.split(separator: "   ").compactMap { Int($0) }
         }
     }
 
     func part1() -> Any {
-        print(entities)
-        return 0
+        // split the locations into to two groups
+        var left = locations.map { $0.first ?? 0 }
+        var right = locations.map { $0.second ?? 0 }
+
+        var totalDistance = 0
+
+        // Find the smallest number and delete it from the list.
+        // Repeat until all the elements are deleted from the list
+        while left.count != 0 {
+            var smallestLeftIndex = 0
+            for index in 0..<left.count {
+                if left[index] < left[smallestLeftIndex] {
+                    smallestLeftIndex = index
+                }
+            }
+
+            var smallestRightIndex = 0
+            for index in 0..<right.count {
+                if right[index] < right[smallestRightIndex] {
+                    smallestRightIndex = index
+                }
+            }
+
+            totalDistance += abs(left[smallestLeftIndex] - right[smallestRightIndex])
+
+            left.remove(at: smallestLeftIndex)
+            right.remove(at: smallestRightIndex)
+        }
+        return totalDistance
     }
 
     func part2() -> Any {
